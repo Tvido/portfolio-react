392f8f81132f858bbd1177ae5536bf235d459b6b
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-05 18:45:48 +0200

    styled nav menu

--- src/sections/NavBar/NavBar.jsx
+++ src/sections/NavBar/NavBar.jsx
@@ -1,8 +1,25 @@
+
+import {CiSettings} from 'react-icons/ci'
 import './navbar.css'
+import logo from '../../assets/img/logo.svg'
+import  data from './data'
 
 const NavBar = () => {
   return (
-    <nav id="navbar"><h2>Some NavBar</h2></nav>
+    <nav id="navbar">
+      <div className="container nav__container">
+        <a href="/" className='nav__logo'>
+          <img src={logo} alt="logo" />
+        </a>
+        <ul className="nav__menu">
+          {
+            data.map(item => <li key={ item.id}><a href={ item.link}>{ item.title}</a></li>)
+          }
+        </ul>
+          
+        <button id='theme__icon' ><CiSettings /></button>
+      </div>
+    </nav>
   )
 }
 
---
+++ src/sections/NavBar/data.js
@@ -0,0 +1,9 @@
+const data = [
+  { id: 1, link: "#", title: "Home" },
+  { id: 2, link: "#about", title: "Aboute" },
+  { id: 3, link: "#portfolio", title: "Portfolio" },
+  { id: 4, link: "#technologies", title: "Technologies" },
+  { id: 5, link: "#contacts", title: "Contacts" },
+];
+
+export default data;
